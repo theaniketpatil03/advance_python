40c08e53f4942790488b0f36e0359b20006f2ee4
Aniket Patil <theaniketpatil03@gmail.com>
2024-06-23 00:28:41 +0530

    practice: numeric type

---
+++ data_types/numeric/complex/practice.py
@@ -0,0 +1,5 @@
+com1 = complex(2, 5)
+com2 = complex(3, 10)
+print(type(com1 + com2))
+
+print(9 +1j)
---
+++ data_types/numeric/decimal_/practice.py
@@ -0,0 +1,10 @@
+from decimal import Decimal
+x = Decimal(10)
+
+print(x)
+
+import decimal
+
+y = decimal.Decimal(2)
+print(dir(y))
+print(y.is_finite)
